e663eb48b871d1dc2a6adfcd6bac3acfc25e204e
hsheikh7 <49715207+hsheikh7@users.noreply.github.com>
2022-09-03 11:44:16 +0430

    Add files via upload

---
+++ BreakTime.py
@@ -0,0 +1,10 @@
+import time 
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+while (total_breaks < break_count )
+    time.sleep(10)  
+    webbrowser.open("https://www.youtube.com/watch?v=zip34r6Ba-g")
+    break_count = break_count + 1 
